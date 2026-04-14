9015dabbe7b3d248ab4d1fa06659a5b2c765260b
nelson alayon <nelson.alayon@gmail.com>
2026-04-14 10:20:14 -0500

    my first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Customers() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1 className="text-4xl font-bold">Welcome to the Customers Page</h1>
+        </main>
+    )
+}
